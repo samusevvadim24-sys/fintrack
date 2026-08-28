a0449f73c83230d1d3e9c74d48a7aa4ac835e6e1
samusevvadim24-sys <samusev.vadim.24@gmail.com>
2026-08-28 23:12:02 +0300

    Allow Android device to load Next.js dev resources

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Allow the development phone/browser origin to load Next.js dev resources.
+  allowedDevOrigins: ["192.168.113.126"],
+};
+
+module.exports = nextConfig;
